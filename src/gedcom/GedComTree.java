fbbbaa8b646a0177c969f03e8bb1926afdd71388
VidyaMaiya <vid04star@gmail.com>
2019-02-11 12:25:15 -0500

    Added the tabular format output for individual and family records

--- src/gedcom/GedComTree.java
+++ src/gedcom/GedComTree.java
@@ -6,24 +6,35 @@ import java.util.TreeMap;
 
 public class GedComTree
 {
-	public static void printIndividualList(FamilyTreeParser Ftp) {
+	private static void printIndividualList(FamilyTreeParser Ftp) {
 		Map<String,Individual> treeMap = new TreeMap<String,Individual>(Ftp.individualMap);
-		System.out.println("------------------INDIVIDUAL-------------------------------");
+		System.out.println("Individuals");
+		String individualOutputFormat = "|%1$-6s|%2$-20s|%3$-10s|%4$-12s|%5$-7s|%6$-9s|%7$-12s|%8$-10s|%9$-10s|%n";
+		System.out.format("+------+--------------------+----------+------------+-------+---------+------------+----------+----------+%n");
+		System.out.format("|  ID  |       Name         |  Gender  |  Birthday  |  Age  |  Alive  |    Death   |   Child  |  Spouse  |%n");
+		System.out.format("+------+--------------------+----------+------------+-------+---------+------------+----------+----------+%n");
 			for(Map.Entry<String, Individual> entry : treeMap.entrySet())
 			{
 				Individual indi = entry.getValue();
-				System.out.println(indi.getId()+"---"+indi.getName()+"---"+indi.getGender()+"---"+indi.getBirthDate()+"---"+indi.getAge()+"---"+indi.isAlive()+"---"+indi.getDeathDate()+"---"+indi.getFamilyChildId()+"---"+indi.getFamilySpouseId());
+				System.out.format(individualOutputFormat, indi.getId(),indi.getName(),indi.getGender(),indi.getBirthDate(),indi.getAge(),indi.isAlive(),indi.getDeathDate()==null?"NA":indi.getDeathDate(),indi.getFamilyChildId()==null?"NA":"{'"+ indi.getFamilyChildId()+"'}",indi.getFamilySpouseId()==null?"NA":"{'"+indi.getFamilySpouseId()+"'}");
+				
 			}
+			System.out.format("+------+--------------------+----------+------------+-------+---------+------------+----------+----------+%n");
 		}
-	public static void printFamilyList(FamilyTreeParser Ftp) {
-		System.out.println("-----------------------------FAMILY-------------------");
+	private static void printFamilyList(FamilyTreeParser Ftp) {
+		System.out.println("Families");
+		String familyOutputFormat = "|%1$-6s|%2$-16s|%3$-16s|%4$-14s|%5$-23s|%6$-14s|%7$-23s|%8$-23s|%n";
+		System.out.format("+------+----------------+----------------+--------------+-----------------------+--------------+-----------------------+-----------------------+%n");
+		System.out.format("|  ID  |    Married     |    Divorced    |  Husband Id  |      Husband Name     |    Wife Id   |      Wife Name        |      Children         |%n");
+		System.out.format("+------+----------------+----------------+--------------+-----------------------+--------------+-----------------------+-----------------------+%n");
 		for(int i=0;i<Ftp.familyList.size();i++)
 		{
 			Family fam = Ftp.familyList.get(i);
 			String wifeName = Ftp.individualMap.get(fam.getWifeId()).getName();
 			String husbandName = Ftp.individualMap.get(fam.getHusbandId()).getName();
-			System.out.println(fam.getId()+"---"+fam.getMarriageDate()+"---"+fam.getDivorcedDate()+"---"+fam.getHusbandId()+"---"+husbandName+"---"+fam.getWifeId()+"---"+wifeName+"---"+fam.getChildId());
+			System.out.format(familyOutputFormat, fam.getId(),fam.getMarriageDate()==null?"NA":fam.getMarriageDate(),fam.getDivorcedDate()==null?"NA":fam.getDivorcedDate(),fam.getHusbandId(),husbandName,fam.getWifeId(),wifeName,fam.getChildId().isEmpty()?"NA":"{'"+String.join("', '", fam.getChildId())+"'}");
 		}
+		System.out.format("+------+----------------+----------------+--------------+-----------------------+--------------+-----------------------+-----------------------+%n");
 	}
 	public static void main(String[] args) throws IOException
 	{
